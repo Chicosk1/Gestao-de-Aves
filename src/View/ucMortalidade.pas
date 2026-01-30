ecc76c84a2e4b9450669f5ff0868dea1286a2519
Cichocki <gabrielsamudiocichocki@gmail.com>
2026-01-30 19:06:42 -0300

    Implementando interface do lançamentos de mortalidade

--- src/View/ucMortalidade.pas
+++ src/View/ucMortalidade.pas
@@ -4,10 +4,23 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls, Vcl.StdCtrls, Vcl.Buttons,
+  Vcl.ComCtrls;
 
 type
   TfrmMortalidade = class(TForm)
+    pnlInfoLote: TPanel;
+    pnlInputs: TPanel;
+    pnlBotoes: TPanel;
+    lblLoteSelecionado: TLabel;
+    btnSalvar: TBitBtn;
+    btnCancelar: TBitBtn;
+    lblData: TLabel;
+    dtpDataMortalidade: TDateTimePicker;
+    Label1: TLabel;
+    edtQuantidade: TEdit;
+    lblObs: TLabel;
+    memObservacao: TMemo;
   private
     { Private declarations }
   public
