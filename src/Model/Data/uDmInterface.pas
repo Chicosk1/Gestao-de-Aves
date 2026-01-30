40072f64183b310dd10059a0873dd85dccc58dd1
Cichocki <gabrielsamudiocichocki@gmail.com>
2026-01-30 19:44:36 -0300

    Implementando interfaces necessárias para manipulação das entidades e conexão com o banco

--- src/Model/Data/uDmInterface.pas
+++ src/Model/Data/uDmInterface.pas
@@ -2,6 +2,26 @@ unit uDmInterface;
 
 interface
 
+uses
+  Data.DB,
+  uInterfaces;
+
+type
+  ILoteDAO = interface
+    ['{6763D563-1768-4504-8902-86101416962A}']
+    function ObterTodos: TDataSet;
+  end;
+
+  IPesagemDAO = interface
+    ['{23206775-4309-4809-9060-70808381123B}']
+    procedure Inserir(Entidade: IPesagem);
+  end;
+
+  IMortalidadeDAO = interface
+    ['{10906233-5601-4190-8022-83908381123C}']
+    function Inserir(Entidade: IMortalidade): Double;
+  end;
+
 implementation
 
 end.
